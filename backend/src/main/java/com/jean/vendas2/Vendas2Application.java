1fb1553fd4590b18271d523b5d862dc0ed3eb48d
jcheavy <jcheavy@hotmail.com>
2021-05-10 23:27:44 -0300

    vendas2

---
+++ backend/src/main/java/com/jean/vendas2/Vendas2Application.java
@@ -0,0 +1,13 @@
+package com.jean.vendas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vendas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vendas2Application.class, args);
+	}
+
+}
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://heavyusers.com.br" target="_blank" rel="noreferrer">HeavyUsers</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Desenvolvido por: <a href="https://heavyusers.com.br" target="_blank" rel="noreferrer">jcheavy@hotmail.com</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
